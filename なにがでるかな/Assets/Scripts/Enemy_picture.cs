5326e0cb4cc5bec12012d1c9e9239b72423f9d46
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2019-10-09 20:13:19 +0900

    test

---
+++ なにがでるかな/Assets/Scripts/Enemy_picture.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_picture : MonoBehaviour
+{
+    SpriteRenderer MainSpriteRenderer;
+
+    public Sprite BroomGhost;
+    public Sprite PumpkinTotem;
+    public Sprite CorpseSeoul;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MainSpriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        ChangeState();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void ChangeState()
+    {
+        MainSpriteRenderer.sprite = BroomGhost;
+    }
+}
