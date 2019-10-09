5f83d2f0d631a32abf8687ae7d2e9c8a1ce94d47
yoshihiro-6894 <54787477+yoshihiro-6894@users.noreply.github.com>
2019-10-09 21:35:44 +0900

    battle

---
+++ なにがでるかな(よっしー)/Assets/Scripts/damagetext.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class damagetext : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(new Vector3(Random.Range(-80, 80), Random.Range(100, 180), 0));
+        StartCoroutine(DestroyObject());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private IEnumerator DestroyObject()
+    {
+        yield return new WaitForSeconds(0.6f);
+        Destroy(this.gameObject);
+    }
+}
