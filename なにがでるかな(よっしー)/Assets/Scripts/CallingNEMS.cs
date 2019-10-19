3fda5a2e684c2a7572abf7a016237fc1ca42df29
newtowel <syelpmaxe@gmail.com>
2019-10-19 18:35:35 +0900

    NewTowelExtendedMagicSkillの参照テスト用csファイルなどを設置

---
+++ なにがでるかな(よっしー)/Assets/Scripts/CallingNEMS.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using QRCodeTranslator;
+
+public class CallingNEMS : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+		var nems = new NewTowelExtendedMagicSkill("qrCodeString", true, "FIRE", Application.streamingAssetsPath+"/spell.db");
+		Debug.Log(Application.streamingAssetsPath);
+		Debug.Log(nems.Spell1);
+		Debug.Log(nems.Spell2);
+		Debug.Log(nems.Power);
+		Debug.Log(nems.Attribute);
+	}
+
+	// Update is called once per frame
+	void Update()
+    {
+        
+    }
+}
