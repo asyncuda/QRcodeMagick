8d33afd69f02e626391635947b9e71098de79b0e
Ashiheart <extrmediamond@yahoo.co.jp>
2020-02-06 03:00:43 +0900

    fix: bug
    バグ修正期間

---
+++ New Unity Project/Assets/triangle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class triangle : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Handles.DrawLine(Vector3.zero, Vector3.right * 10);
+
+        Handles.DrawLine(Vector3.zero, Vector3.left * 10);
+
+        Handles.DrawLine(Vector3.right * 10, Vector3.left * 10);
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ The_Typing_of_Softmedia/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class TypeMatching 
+{
+    public TypeMatching(string t)
+    {
+        TypeText = t;
+    }
+
+    public string TypeText { get; }
+
+    public bool Match(char c)
+    {
+        
+
+        return false;
+    }
+
+}
