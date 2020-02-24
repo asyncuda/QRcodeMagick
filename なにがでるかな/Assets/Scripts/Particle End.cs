c6efc085997d06f24d80018249079714aba89cbc
paradaisuikezaki <53808542+paradaisuikezaki@users.noreply.github.com>
2020-02-24 14:36:11 +0900

    Particle end Script
    make healer particle system

---
+++ なにがでるかな/Assets/Scripts/Particle End.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleEnd : MonoBehaviour
+{
+     ParticleSystem particle;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        particle = this.GetComponent<particleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(particle = isStopped)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
